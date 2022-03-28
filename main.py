582a65743cde703c7e3cad73246fbebe6ebf73fe
Luca Braglia <lbraglia@gmail.com>
2022-03-28 11:32:39 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
+import os
 import numpy as np
 import pandas as pd
 import xlsxwriter
 
+debug = True
+
 # numero di righe per cui è possibile inserire dati. Le altre son bloccate
 n_fillable_rows = 10000 
 protection_pw = 'asd'
-
-# def excel_col(n):
-#     # https://stackoverflow.com/questions/19153462
-#     excel_col = str()
-#     div = n
-#     while div:
-#         (div, mod) = divmod(div-1, 26) # will return (x, 0 .. 25)
-#         excel_col = chr(mod + 65) + excel_col
-#     return excel_col
 
 def import_validate(x):
     if (pd.isna(x)):
@@ -45,26 +39,26 @@ def import_sino(x):
     else:
         return x == 'Sì'
 
-def import_id_elenco(x):
+def import_id_elenco(x, modalita):
     if (pd.isna(x)):
         return x
     else:
-        return x
-
+        return modalita[x]
         
     
 class Column:
-    def __init__(self, prog, struct, modalita, debug = True):
+    def __init__(self, prog, struct, modalita, debug = debug):
         # posizione, nome variabile e descrizione
         self.index         = prog
         self.variable      = struct['variabile']
         self.description   = struct['descrizione_e_unita_misura']
+        # debug info
         if (debug):
             print("importing " + self.variable)
         # data validation for excel
         # gestione delle domande a risposta multipla se specificato
         # prendile da modalita, se no imposta a np.nan
-        source_modalita = import_id_elenco(struct['id_elenco'])
+        source_modalita = import_id_elenco(struct['id_elenco'], modalita)
         self.validation = {
             'validate'      : import_validate(struct['tipo']),
             'source'        : source_modalita,
@@ -73,7 +67,7 @@ class Column:
             'minimum'       : struct['minimo'],
             'maximum'       : struct['massimo'],
             'ignore_blank'  : import_sino(struct['ignora_celle_vuote']),
-            'drop_down'     : import_sino(struct['elenco_nella_cella']),
+            'dropdown'      : import_sino(struct['elenco_nella_cella']),
             'show_input'    : import_sino(struct['input_mostra']),
             'input_title'   : struct['input_titolo'],
             'input_message' : struct['input_messaggio'],
@@ -83,7 +77,7 @@ class Column:
             'error_message' : struct['errore_messaggio']
         }
 
-    def export(self, ws, debug = True):
+    def export(self, ws, debug = debug):
         prog_col = self.index
         # title
         ws.write(0, prog_col, self.variable)
@@ -91,16 +85,23 @@ class Column:
             print("exporting " + self.variable)
         # data validation from 1 to fillable_rows
         validation_dict = {
-            d:v for d,v in self.validation.items() if not pd.isna(v)
+            # tieni se è una lista (per le modalità) o se non è NA
+            d:v for d,v in self.validation.items()
+            if isinstance(v, list) or not pd.isna(v)
         }
+        # debug info
+        if (debug):
+            print(validation_dict)
         ws.data_validation(1, prog_col, n_fillable_rows, prog_col,
                            validation_dict)
 
         
 class Sheet:
-    def __init__(self, xl, sheetname, modalita):
+    def __init__(self, xl, sheetname, modalita, debug = debug):
         sheet = xl.parse(sheetname)
-        sheet = sheet.reset_index() 
+        sheet = sheet.reset_index()
+        if (debug):
+            print(sheet)
         self.sheetname = sheetname
         # estrai i dati delle colonne dell'output
         # ciclando sulle righe del data.frame di input
@@ -108,10 +109,10 @@ class Sheet:
         for index, row in sheet.iterrows():
             self.columns.append(Column(index, row, modalita))
 
-    def export(self, ws, formats):
+    def export(self, ws, formats, debug = debug):
         # Data
         for c in self.columns:
-            c.export(ws, modalita)
+            c.export(ws)
         # formatting the worksheet
         ws.set_row(0, cell_format = formats['title'])
         # blocca riquadri
@@ -137,7 +138,7 @@ class CRF:
         # a CRF is a dict of sheets (each called by its name)
         self.sheets = {}
         
-    def read_structure(self, f):
+    def read_structure(self, f, debug = debug):
         """
         Import a dataset structure
         """
@@ -152,9 +153,8 @@ class CRF:
                        ['modalita_output', 'modalita_struttura']]
         for s in data_sheets:
             self.sheets[s] = Sheet(xl, s, modalita)
-        
 
-    def create(self, f):
+    def create(self, f, debug = debug):
         """
         Create an xlsx template according to the structure
         """
@@ -171,11 +171,15 @@ class CRF:
         wb.close()
 
 
-
+def script(inpath):
+    outdir = '/tmp'
+    outfile = os.path.basename(os.path.splitext(inpath)[0] + "_CRF.xlsx")
+    outpath = os.path.join(outdir, outfile)
+    ex1 = CRF()
+    ex1.read_structure(inpath)
+    ex1.create(outpath)
+    print("output file: "+ outpath)
+        
     
 if __name__ == '__main__':
-    infile = '/home/l/xlcrf/structs/example1.xlsx'
-    outfile = '/tmp/example1_crf.xlsx'
-    ex1 = CRF()
-    ex1.read_structure(infile)
-    ex1.create(outfile)
+    script("/home/l/xlcrf/examples/example1.xlsx")
